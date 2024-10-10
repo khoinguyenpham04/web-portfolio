a77ed0fd75042f5cfa1ef71b1edc04774ef59d86
Noah (Pham Tran Khoi Nguyen) <ptknguyen04@gmail.com>
2024-10-10 16:44:15 +0100

    Initial commit

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,140 @@
+import "@/once-ui/styles/index.scss";
+import "@/once-ui/tokens/index.scss";
+
+import classNames from 'classnames';
+
+import { Flex, Background } from '@/once-ui/components'
+import { Footer, Header, RouteGuard } from "@/components";
+import { baseURL, effects, style } from '@/app/resources'
+
+import { Lexend, Montserrat, Manrope, Source_Code_Pro } from 'next/font/google';
+
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import { Metadata } from "next";
+import { routing } from "@/i18n/routing";
+import { renderContent } from "@/app/resources";
+
+export async function generateMetadata(
+    { params: { locale }}: { params: { locale: string }}
+) {
+
+    const t = await getTranslations();
+    const { person, home } = renderContent(t);
+
+    return {
+        metadataBase: new URL(`https://${baseURL}/${locale}`),
+        title: home.title,
+        description: home.description,
+        openGraph: {
+            title: `${person.firstName}'s Portfolio`,
+            description: 'Portfolio website showcasing my work.',
+            url: baseURL,
+            siteName: `${person.firstName}'s Portfolio`,
+            locale: 'en_US',
+            type: 'website',
+        },
+        robots: {
+            index: true,
+            follow: true,
+            googleBot: {
+                index: true,
+                follow: true,
+                'max-video-preview': -1,
+                'max-image-preview': 'large',
+                'max-snippet': -1,
+            },
+        },
+    }
+};
+
+const primary = Lexend({
+    variable: '--font-primary',
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+const secondary = Montserrat({
+    variable: '--font-secondary',
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+const tertiary = Manrope({
+    variable: '--font-tertiary',
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+const code = Source_Code_Pro({
+    variable: '--font-code',
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+interface RootLayoutProps {
+    children: React.ReactNode;
+    params: {locale: string};
+}
+
+export function generateStaticParams() {
+    return routing.locales.map((locale) => ({locale}));
+  }
+
+export default async function RootLayout({
+    children,
+    params: {locale}
+} : RootLayoutProps) {
+    unstable_setRequestLocale(locale);
+    const messages = await getMessages();
+    return (
+        <NextIntlClientProvider messages={messages}>
+            <Flex
+                as="html" lang="en"
+                background="page"
+                data-theme="dark"
+                data-brand="indigo"
+                data-accent="cyan"
+                data-neutral="gray"
+                data-border="playful"
+                data-solid="contrast"
+                data-solid-style="flat"
+                data-surface="translucent"
+                data-transition="all"
+                className={classNames(
+                    primary.variable,
+                    secondary.variable,
+                    tertiary.variable,
+                    code.variable)}>
+                <Flex style={{minHeight: '100vh'}}
+                    as="body"
+                    fillWidth margin="0" padding="0"
+                    direction="column">
+                    <Background
+                        gradient={effects.gradient}
+                        dots={effects.dots}
+                        lines={effects.lines}/>
+                    <Flex
+                        fillWidth
+                        minHeight="16">
+                    </Flex>
+                    <Header/>
+                    <Flex
+                        zIndex={0}
+                        fillWidth paddingY="l" paddingX="l"
+                        justifyContent="center" flex={1}>
+                        <Flex
+                            justifyContent="center"
+                            fillWidth minHeight="0">
+                            <RouteGuard>
+                                {children}
+                            </RouteGuard>
+                        </Flex>
+                    </Flex>
+                    <Footer/>
+                </Flex>
+            </Flex>
+        </NextIntlClientProvider>
+    );
+}
---
+++ src/app/resources/content.js
@@ -0,0 +1,272 @@
+import { InlineCode } from "@/once-ui/components";
+
+const person = {
+    firstName: 'Noah',
+    lastName:  'Pham',
+    get name() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    role:      'Software Engineer',
+    avatar:    '/images/avatar.jpg',
+    location:  'Europe/London',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    languages: ['English']  // optional: Leave the array empty if you don't want to display languages
+}
+
+const newsletter = {
+    display: true,
+    title: <>Subscribe to {person.firstName}'s Newsletter</>,
+    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity and engineering.</>
+}
+
+const social = [
+    // Links are automatically displayed.
+    // Import new icons in /once-ui/icons.ts
+    {
+        name: 'GitHub',
+        icon: 'github',
+        link: 'https://github.com/khoinguyenpham04',
+    },
+    {
+        name: 'LinkedIn',
+        icon: 'linkedin',
+        link: 'https://www.linkedin.com/in/phamtrankhoinguyen-noah/',
+    },
+    {
+        name: 'X',
+        icon: 'x',
+        link: '',
+    },
+    {
+        name: 'Email',
+        icon: 'email',
+        link: 'mailto:ptknguyen04@gmail.com',
+    },
+]
+
+const home = {
+    label: 'Home',
+    title: `${person.name}'s Portfolio`,
+    description: `Portfolio website showcasing my work as a ${person.role}`,
+    headline: <>From Code to Impact: Crafting User-Centric Solutions</>,
+    subline: <>I'm Noah, a computer science at <InlineCode>The University of Manchester</InlineCode>, where I learn and build intuitive and impactful applications.</>
+}
+
+const about = {
+    label: 'About',
+    title: 'About me',
+    description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+    tableOfContent: {
+        display: true,
+        subItems: true
+    },
+    avatar: {
+        display: true
+    },
+    calendar: {
+        display: true,
+        link: 'https://cal.com'
+    },
+    intro: {
+        display: true,
+        title: 'Introduction',
+        description: <>Noah is a Manchester-based software engineer with a passion for transforming complex challenges into simple, elegant design solutions. His work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
+    },
+    work: {
+        display: true, // set to false to hide this section
+        title: 'Work Experience',
+        experiences: [
+            {
+                company: 'HyperLoop Manchester',
+                timeframe: 'September 2023 - Present',
+                role: 'Electronics and Software Engineer',
+                achievements: [
+                    <>Programmed and designed a multi-panel GUI using C++ and Qt Widgets to display real-time camera feeds, motor variables, GPS location data, 3D pod heat maps, and system logs.</>,
+                    <>Established real-time communication between the micro-controllers and GUI to ensure seamless sensor updates.</>,
+                    <>Leveraged HTML, CSS and JavaScript to enhance the front-end user experience and modernised the design architectureof HyperLoop’s main website through front-end development.</>,
+                    <>Summarised and presented send weekly reports and documentation of contributions to team members using LATEX and Microsoft Office tools.</>
+                    
+                ],
+                images: [ // optional: leave the array empty if you don't want to display images
+                    {
+                        src: '/images/projects/project-01/cover-06.jpeg',
+                        alt: 'Once UI Project',
+                        width: 16,
+                        height: 9
+                    }
+                ]
+            },
+            {
+                company: 'The University of Manchester',
+                timeframe: 'September 2023 - Present',
+                role: 'Student Representative for the Department of Computer Science',
+                achievements: [
+                    <>Elected to represent over 400 students, facilitated peer communication and utilized surveys for feedback.</>,
+                    <>Successfully conveyed student feedback and concerns during regular Student Liaison Committee meetings constructively.</>,
+                ],
+                images: [ ]
+            }
+        ]
+    },
+    studies: {
+        display: true, // set to false to hide this section
+        title: 'Studies',
+        institutions: [
+            {
+            name: 'University of Manchester - BSc Computer Science',
+            description: [<>
+                • Relevant Coursework: Data Science, Operating System, Computer Engineering & Architecture, Discrete Mathematics.
+                • Activities: Society, GreatUniHack 23’, Hackchester - Cyber Security, Manchester Trading Society and MUDSS.
+
+            </>,],
+            },
+        ]
+        },
+        technical: {
+        display: false, // set to false to hide this section
+        title: 'Technical skills',
+        skills: [
+            {
+                title: 'Figma',
+                description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+                images: [
+                    {
+                        src: '/images/projects/project-01/cover-02.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                    {
+                        src: '/images/projects/project-01/cover-03.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                ]
+            },
+            {
+                title: 'Next.js',
+                description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+                images: [
+                    {
+                        src: '/images/projects/project-01/cover-04.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                ]
+            }
+        ]
+    }
+}
+
+const blog = {
+    label: 'Blog',
+    title: 'Writing about design and tech...',
+    description: `Read what ${person.name} has been up to recently`
+    // Create new blog posts by adding a new .mdx file to app/blog/posts
+    // All posts will be listed on the /blog route
+}
+
+const work = {
+    label: 'Work',
+    title: 'My projects',
+    description: `Design and dev projects by ${person.name}`
+    // Create new project pages by adding a new .mdx file to app/blog/posts
+    // All projects will be listed on the /home and /work routes
+}
+
+const gallery = {
+    label: 'Gallery',
+    title: 'My photo gallery',
+    description: `A photo collection by ${person.name}`,
+    // Images from https://pexels.com
+    images: [
+        { 
+            src: '/images/gallery/img-01.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-2.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-3.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-04.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-05.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-6.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-7.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-8.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-09.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-10.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-11.jpeg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-12.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-13.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-14.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-15.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-16.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-17.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+    ]
+}
+
+export { person, social, newsletter, home, about, blog, work, gallery };
